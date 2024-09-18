a9e7334fd41e953148e7928b3e344761f62def05
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 23:44:02 +0300

    Implement Insertion Sort algorithm for doubly linked list

--- sort.h
+++ sort.h
@@ -3,7 +3,25 @@
 
 #include <stddef.h>
 
-void bubble_sort(int *array, size_t size);
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
 
 #endif /* SORT_H */
