8ab2f2f8b9c93c9d08839cebc995c9a86579d040
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 23:38:36 +0300

    Implement Insertion Sort algorithm for doubly linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swaps two adjacent nodes in a doubly linked list
+ * @list: Pointer to the head of the doubly linked list
+ * @left: Pointer to the node on the left to swap
+ * @right: Pointer to the node on the right to swap
+ */
+void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+{
+    if (left->prev)
+        left->prev->next = right;
+    else
+        *list = right;
+    if (right->next)
+        right->next->prev = left;
+
+    left->next = right->next;
+    right->prev = left->prev;
+    right->next = left;
+    left->prev = right;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ *                       order using the Insertion sort algorithm
+ * @list: Double pointer to the head of the list
+ */
+void insertion_sort_list(lis
