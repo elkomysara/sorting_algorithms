3ea6ae3095a5540251193a5f98b4bbef00aa7669
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 23:42:05 +0300

    Implement Insertion Sort algorithm for doubly linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,17 +8,17 @@
  */
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 {
-    if (left->prev)
-        left->prev->next = right;
-    else
-        *list = right;
-    if (right->next)
-        right->next->prev = left;
+	if (left->prev)
+		left->prev->next = right;
+	else
+		*list = right;
+	if (right->next)
+		right->next->prev = left;
 
-    left->next = right->next;
-    right->prev = left->prev;
-    right->next = left;
-    left->prev = right;
+	left->next = right->next;
+	right->prev = left->prev;
+	right->next = left;
+	left->prev = right;
 }
 
 /**
@@ -26,4 +26,23 @@ void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
  *                       order using the Insertion sort algorithm
  * @list: Double pointer to the head of the list
  */
-void insertion_sort_list(lis
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *next, *tmp;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		next = current->next;
+		tmp = current;
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			swap_nodes(list, tmp->prev, tmp);
+			print_list(*list);
+		}
+		current = next;
+	}
+}
