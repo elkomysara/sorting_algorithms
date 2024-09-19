ac73b212aada1afc61270eb202e9022919f187b3
elkomysara <sa1244@fayoum.edu.eg>
2024-09-19 06:42:46 +0300

    Implement Selection Sort algorithm

--- sort.h
+++ sort.h
@@ -12,16 +12,15 @@
  */
 typedef struct listint_s
 {
-    int n;
+    const int n;
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
+void selection_sort(int *array, size_t size);  /* Added prototype */
 
 #endif /* SORT_H */
