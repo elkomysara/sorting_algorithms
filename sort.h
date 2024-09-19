c7ada02a313adb7675cb1c03b4d25d716c6c140f
elkomysara <sa1244@fayoum.edu.eg>
2024-09-19 07:04:34 +0300

    Implement Shell Sort using Knuth sequence

--- sort.h
+++ sort.h
@@ -2,16 +2,15 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* struct listint_s - Doubly linked list node structure
+* struct listint_s - Doubly linked list node
 *
 * @n: Integer stored in the node
 * @prev: Pointer to the previous element of the list
 * @next: Pointer to the next element of the list
-*
-* Description: This structure is used for the doubly linked list
-* in sorting algorithms.
 */
 typedef struct listint_s
 {
@@ -20,20 +19,14 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-/* Function prototypes for sorting algorithms */
-
-/**
-* quick_sort - Sorts an array of integers in ascending order
-*              using the Quick sort algorithm (Lomuto partition scheme).
-* @array: Pointer to the array of integers.
-* @size: The size of the array.
-*
-* Description: Implements the Quick sort algorithm with Lomuto partitioning,
-* printing the array after every swap.
-*/
-void quick_sort(int *array, size_t size);
-
+/* Prototypes for sorting algorithms */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size); /* Added Shell Sort prototype */
 
 #endif /* SORT_H */
