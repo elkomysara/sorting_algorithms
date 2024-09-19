cc49a2f4f2c61bd09a5f22e418495087c9076494
elkomysara <sa1244@fayoum.edu.eg>
2024-09-19 06:58:27 +0300

    Implement Quick Sort algorithm and add Big O notation in 3-O

--- sort.h
+++ sort.h
@@ -4,33 +4,33 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - Doubly linked list node structure
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- *
- * Description: This structure is used for the doubly linked list
- * in sorting algorithms.
- */
+* struct listint_s - Doubly linked list node structure
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*
+* Description: This structure is used for the doubly linked list
+* in sorting algorithms.
+*/
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /* Function prototypes for sorting algorithms */
 
 /**
- * quick_sort - Sorts an array of integers in ascending order
- *              using the Quick sort algorithm (Lomuto partition scheme).
- * @array: Pointer to the array of integers.
- * @size: The size of the array.
- *
- * Description: Implements the Quick sort algorithm with Lomuto partitioning,
- * printing the array after every swap.
- */
+* quick_sort - Sorts an array of integers in ascending order
+*              using the Quick sort algorithm (Lomuto partition scheme).
+* @array: Pointer to the array of integers.
+* @size: The size of the array.
+*
+* Description: Implements the Quick sort algorithm with Lomuto partitioning,
+* printing the array after every swap.
+*/
 void quick_sort(int *array, size_t size);
 
 void print_array(const int *array, size_t size);
